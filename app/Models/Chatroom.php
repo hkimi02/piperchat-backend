40edd3c1020c54bccbbe4b0e26bb3782c9ffa84b
elyescesar <ilyesghorbel69@gmail.com>
2025-06-12 22:11:08 +0100

    chat working but buggy

--- app/Models/Chatroom.php
+++ app/Models/Chatroom.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chatroom extends Model
@@ -24,5 +25,10 @@ class Chatroom extends Model
     public function messages()
     {
         return $this->hasMany(Message::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'chatroom_user');
     }
 }
